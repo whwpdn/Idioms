1f85e0283bd6ccec1c697b62a22558214d777c43
jewoo <whwpdn@gmail.com>
2016-07-08 22:39:10 +1000

    add IdomsData class
    1.add IdomsData class
    2. modify db

---
+++ app/src/main/java/com/example/jewoo/idoms/IdomsData.java
@@ -0,0 +1,46 @@
+package com.example.jewoo.idoms;
+
+/**
+ * Created by jewoo on 2016. 7. 8..
+ */
+public class IdomsData {
+    private int mId;
+    private String mQuestion;
+    private String mAnswer;
+
+    public IdomsData(int mId, String mQuestion, String mAnswer) {
+        this.mId = mId;
+        this.mQuestion = mQuestion;
+        this.mAnswer = mAnswer;
+    }
+
+    public int getmId() {
+
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+
+    public String getmQuestion() {
+        return mQuestion;
+    }
+
+    public String getmAnswer() {
+        return mAnswer;
+    }
+//    public void setmQuestion(String mQuestion) {
+//        this.mQuestion = mQuestion;
+//    }
+
+
+
+//    public void setmAnswer(String mAnswer) {
+//        this.mAnswer = mAnswer;
+//    }
+
+
+
+
+}
