54490f64ba58aa276b5b1027213f92e0ca75d60a
jewoo <whwpdn@gmail.com>
2017-03-13 16:59:59 +1000

    add Listening
    -sample files
    -listening fragment

--- app/src/main/java/jewoo/idioms/MainActivity.java
+++ app/src/main/java/jewoo/idioms/MainActivity.java
@@ -1,9 +1,12 @@
 package jewoo.idioms;
 
 
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.os.Environment;
 import android.support.design.widget.TabLayout;
 
 import android.support.v4.view.ViewPager;
@@ -19,6 +22,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TabHost;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements IdiomsFragment.On
     private boolean mSelectedMode = false;
     private int mSelectedId =0;
 
+    private SharedPreferences spf;
     // level
     private int mCurrentLevelId =1; // default 1
 
@@ -63,6 +72,7 @@ public class MainActivity extends AppCompatActivity implements IdiomsFragment.On
 //            }
 //        });
 
+        checkFirstRun();
         TabHost host = (TabHost)findViewById(R.id.tabHost);
         host.setup();
         //Tab 1
@@ -75,6 +85,12 @@ public class MainActivity extends AppCompatActivity implements IdiomsFragment.On
         spec = host.newTabSpec("Patterns");
         spec.setContent(R.id.tabPatterns);
         spec.setIndicator("Patterns");
+        host.addTab(spec);
+
+        //tab 3
+        spec = host.newTabSpec("Listening");
+        spec.setContent(R.id.tabListening);
+        spec.setIndicator("Listening");
         host.addTab(spec);
 
         mListIdoms = new ArrayList<IdiomsData>();
@@ -316,4 +332,120 @@ public class MainActivity extends AppCompatActivity implements IdiomsFragment.On
         mListIdoms.clear();
     }
 
+    private void CopyAssets() {
+        AssetManager assetManager = getResources().getAssets();//getAssets();
+
+        String[] files = null;
+        String mkdir = null ;
+        try {
+            files = assetManager.list("Samples");
+
+            //이미지만 가져올때 files = assetManager.list("image");
+
+        } catch (IOException e) {
+            Log.e("tag", e.getMessage());
+        }
+        for(int i=0; i<files.length; i++) {
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+                in = assetManager.open("Samples/"+files[i]);
+                //in = assetManager.open(files[i]);
+
+                //폴더생성
+                String str = Environment.getExternalStorageState();
+                if ( str.equals(Environment.MEDIA_MOUNTED)) {
+                    mkdir = getExternalFilesDir(null).getAbsolutePath()+"/Samples/";
+                    //mkdir = "/sdcard/elecgal/templet/" ;
+                } else {
+                    Environment.getExternalStorageDirectory();
+                }
+                File mpath = new File(mkdir);
+                if(! mpath.isDirectory()) {
+                    mpath.mkdirs();
+                }
+                //
+
+                //out = new FileOutputStream("/sdcard/elecgal/templet/" + files[i]);
+                out = new FileOutputStream(mkdir + files[i]);
+                copyFile(in, out);
+            } catch(Exception e) {
+                Log.e("tag", e.getMessage());
+            }
+            finally{
+                if(in != null){
+                    try{
+                        in.close();
+                        in = null;
+                    }
+                    catch(IOException e){
+
+                    }
+                }
+                if(out != null){
+                    try{
+                        out.flush();
+                        out.close();
+                        out = null;
+                    }catch(IOException e){
+
+                    }
+                }
+            }
+
+        }
+    }
+
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+
+    }
+
+    private void checkFirstRun() {
+
+        final String PREFS_NAME = "installed";
+        final String PREF_VERSION_CODE_KEY = "4";
+        final int DOESNT_EXIST = -1;
+
+
+        // Get current version code
+        int currentVersionCode = 0;
+        try {
+            currentVersionCode = getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
+        } catch (android.content.pm.PackageManager.NameNotFoundException e) {
+            // handle exception
+            e.printStackTrace();
+            return;
+        }
+
+        // Get saved version code
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        int savedVersionCode = prefs.getInt(PREF_VERSION_CODE_KEY, DOESNT_EXIST);
+
+        // Check for first run or upgrade
+        if (currentVersionCode == savedVersionCode) {
+
+            // This is just a normal run
+            return;
+
+        } else if (savedVersionCode == DOESNT_EXIST) {
+
+            // TODO This is a new install (or the user cleared the shared preferences)
+            CopyAssets();
+
+        } else if (currentVersionCode > savedVersionCode) {
+
+            // TODO This is an upgrade
+            CopyAssets();
+
+        }
+
+        // Update the shared preferences with the current version code
+        prefs.edit().putInt(PREF_VERSION_CODE_KEY, currentVersionCode).commit();
+
+    }
 }
